530f8ce556f85376cc79c203a907eeebf0c2d902
ZhouChenghan <501276687@qq.com>
2021-12-03 10:22:27 +0100

    ch1_format

---
+++ RL_CPP_implementation/lab1/ch1_lab.cpp
@@ -0,0 +1,17 @@
+/* std lib definition */
+#include <iostream>
+using namespace std;
+
+/* user lib definition */
+
+/* user variable definition */
+
+/* user constant definition */
+
+int main()
+{
+    cout<<"Program start!"<<endl;
+
+    cout<<"Program end!"<<endl;
+    return 0;
+}
